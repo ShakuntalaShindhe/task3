425ee737733a87bd90fd8fa8da20e64483d62f3f
Shakuntala <shakuntalashindhe@gmail.com>
2025-01-03 20:43:07 +0530

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const productRoutes = require("./routes/productRoutes")
-const cors = require("cors")
+const productRoutes = require("./routes/productRoutes");
+const cors = require("cors");
+require("dotenv").config(); 
 
-const app = express()
-const PORT = 1008
+const app = express();
+const PORT = process.env.PORT || 3000; 
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-mongoose.connect('mongodb+srv://shakuntalashindhe:shindhe@cluster0.xegmm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
+mongoose.connect(process.env.MONGODB_URI, {
+})
+.then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((error) => {
+  console.error("Error connecting to MongoDB:", error.message);
+})
+
 app.use("/api", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-});
+})
